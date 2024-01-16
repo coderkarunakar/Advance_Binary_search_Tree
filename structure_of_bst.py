0857c5824da3499946082cbf615e210c1c7be853
Karunakar chembeti <114292518+coderkarunakar@users.noreply.github.com>
2024-01-16 08:48:45 +0530

    search and print

--- structure_of_bst.py
+++ structure_of_bst.py
@@ -9,11 +9,42 @@ class BST:
         #initially keeping root as None,and no of nodes also zero
         self.root = None
         self.numofnodes = 0
-    def printTree(self):
-        return
+    def printTreeHelper(self,root):
         
+        #note:if you want to call recursively on left and right side then you need to pass root as your argument
+
+        if root == None:
+            return
+        print(root.data,end=':')
+        if root.left != None:
+            print("L",root.left.data,end=',')
+        if root.right != None:
+            print('R',root.right.data,end='')
+        print()
+        printTreeHelper(root.left)
+        printTreeHelper(root.right)
+
+    def printTree(self):
+        printTreeHelper(self.root)
+
+    def isdatapresentHelper(self,root,data):
+        #if the tree is None simply return Fals
+            if root ==  None:
+                return False
+                #if the searching data is equal to the root data then it is true
+            if root.data == data:
+                return True
+            if root.data > data:
+                #call on left
+                return isdatapresentHelper(root.left,data)
+            else:
+                #call on right
+                return isdatapresentHelper(root.right,data)
+        
+        #THIS IS for searching the data
     def isdatapresent(self,data):
-        return False
+        return isdatapresentHelper(self.root,data)
+
     def insert(self,data):
         return
     def deletedata(self,data):
